1165ef30811e99eae0157fef1ea206772028cb25
gomdopi <io.gomdopi@gmail.com>
2025-02-09 10:53:03 -0800

    travel journal header challenge

---
+++ src/components/Header.tsx
@@ -0,0 +1,8 @@
+export default function Header() {
+  return (
+    <header className="header">
+      <img src="globe.png" alt="Globe logo" />
+      <h1>my travel journal.</h1>
+    </header>
+  )
+}
