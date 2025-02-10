3f39fbe23005487ef80e10e56278f968774cb907
gomdopi <io.gomdopi@gmail.com>
2025-02-09 20:09:27 -0800

    passing props with spread operator

--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,7 @@ import entries from "../data.ts"
 
 export default function App() {
   const journalEntries = entries.map((entry) => (
-    <Entry
-      key={entry.id}
-      img={entry.img}
-      title={entry.title}
-      country={entry.country}
-      googleMapsLink={entry.googleMapsLink}
-      dates={entry.dates}
-      text={entry.text}
-    />
+    <Entry key={entry.id} {...entry} />
   ))
   return (
     <>
