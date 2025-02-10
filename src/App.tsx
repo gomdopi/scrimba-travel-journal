e49385f10f693fbcda78e299217205fa52d10026
gomdopi <io.gomdopi@gmail.com>
2025-02-09 18:55:21 -0800

    pass props to entry challenge

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,21 @@
-// import Entry from "./components/Entry"
-// import Header from "./components/Header"
-
-import Joke from "./components/Joke"
-
-// export default function App() {
-//   return (
-//     <>
-//       <Header />
-//       <Entry />
-//     </>
-//   )
-// }
+import Entry from "./components/Entry"
+import Header from "./components/Header"
 
 export default function App() {
   return (
     <>
-      <Joke
-        setup="I got my daughter a fridge for her birthday."
-        punchline="I can't wait to see her face light up when she opens it."
+      <Header />
+      <Entry
+        img={{
+          src: "https://scrimba.com/links/travel-journal-japan-image-url",
+          alt: "Mount Fuji",
+        }}
+        title="Mount Fuji"
+        country="Japan"
+        googleMapsLink="https://www.google.com/maps/place/Mount+Fuji/@35.3606421,138.7170637,15z/data=!3m1!4b1!4m6!3m5!1s0x6019629a42fdc899:0xa6a1fcc916f3a4df!8m2!3d35.3606255!4d138.7273634!16zL20vMGNrczA?entry=ttu"
+        dates="12 Jan, 2021 - 24 Jan, 2021"
+        text="Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists."
       />
-      <Joke
-        setup="How did the hacker escape the police?"
-        punchline="He just ransomware!"
-      />
-      <Joke
-        setup="Why don't pirates travel on mountain roads?"
-        punchline="Scurvy."
-      />
-      <Joke
-        setup="Why do bees stay in the hive in the winter?"
-        punchline="Swarm."
-      />
-      <Joke
-        setup="What's the best thing about Switzerland?"
-        punchline="I don't know, but the flag is a big plus!"
-      />
-      <Joke punchline="It's hard to explain puns to kleptomaniacs because they always take things literally." />
     </>
   )
 }
--- src/components/Entry.tsx
+++ src/components/Entry.tsx
@@ -1,26 +1,23 @@
-export default function Entry() {
+export default function Entry(props: {
+  img: { src: string; alt: string }
+  title: string
+  country: string
+  googleMapsLink: string
+  dates: string
+  text: string
+}) {
   return (
     <article className="journal-entry">
       <div className="main-image-container">
-        <img
-          src="https://s3-alpha-sig.figma.com/img/188d/a2c4/e143f205acb5ae4dc702b16f370e7994?Expires=1739750400&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=qJ4kk5savTvlK5QZwAf-xoQdxpSZPcqM9h5gC-XymCCyHWLoo76aqW5-6d8EyZBAm1Am4aE2tG3XkH~HDkF2vV0fGXG7AXnlkbC7U4e~m7UmUIFyrCD0tdakx~waHmRuM6WcYUC0VzL-S8bT4vimlTq9vXVIHkL4UJxGny3a~MH1ERlzE3TtxZ5vrJEvLY1bvY8RMI9lX-q66vioP8EqH~KTF7vPZCqwLAhLnXKKKcORlBN6gkEW8xcdsViFAmKFHyD~r73mS8r40ujwdThNWoGndbHQf-cLyQZLbF0DhLWiMBlgQ122SCzJ6VE5uqDu2BVTdcU6PKeG10LJAqTHSQ__"
-          alt="mount fuji"
-          className="main-image"
-        />
+        <img src={props.img.src} alt={props.img.alt} className="main-image" />
       </div>
       <div className="info-container">
         <img src="marker.png" alt="map marker icon" className="marker-icon" />
-        <span className="entry-location">JAPAN</span>
-        <a href="https://www.google.com/maps/place/Mount+Fuji/@35.3606421,138.7170637,15z/data=!3m1!4b1!4m6!3m5!1s0x6019629a42fdc899:0xa6a1fcc916f3a4df!8m2!3d35.3606255!4d138.7273634!16zL20vMGNrczA?entry=ttu">
-          View on Google Maps
-        </a>
-        <h1 className="entry-title">Mount Fuji</h1>
-        <p className="entry-dates">12 Jan, 2021 - 24 Jan, 2021</p>
-        <p className="entry-text">
-          Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters
-          (12,380 feet). Mount Fuji is the single most popular tourist site in
-          Japan, for both Japanese and foreign tourists.
-        </p>
+        <span className="entry-location">{props.country.toUpperCase()}</span>
+        <a href={props.googleMapsLink}>View on Google Maps</a>
+        <h1 className="entry-title">{props.title}</h1>
+        <p className="entry-dates">{props.dates}</p>
+        <p className="entry-text">{props.text}</p>
       </div>
     </article>
   )
